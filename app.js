5f86fdd6568fdfc7a9178a5a43cbb38d651eff7e
Elexicon <Emarulos@gmail.com>
2021-10-12 19:51:54 -0300

    First Purification

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ if (pwd == "asdasd"){
     const div = document.querySelector(".main2");
     div.innerHTML = "<strong> Contraseña Inválida </strong>  <br><br> <div> Por favor intente nuevamente</div>";
 
-    const asd = document.querySelector(".invalide");
-    asd.innerHTML = "<strong class='probando'a> Acceso solamente para el área de sistemas </strong> ";
 
 
 
