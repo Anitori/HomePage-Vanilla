738b50bc0fac2a910b15a517ba7ca4468a1de751
Elexicon <Emarulos@gmail.com>
2021-10-12 01:58:01 -0300

    Only Structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HomePage-Vanilla
+# HomePage
+ 
---
+++ app.js
@@ -0,0 +1,20 @@
+let pwd = prompt("Ingrese la contraseña para acceder");
+
+if (pwd == "asdasd"){
+    const div = document.querySelector(".info");
+    div.innerHTML = "";
+
+} else (pwd != "asdasd");{
+    const div = document.querySelector(".main2");
+    div.innerHTML = "<strong> Contraseña Inválida </strong>  <br><br> <div> Por favor intente nuevamente</div>";
+
+    const asd = document.querySelector(".invalide");
+    asd.innerHTML = "<strong class='probando'a> Acceso solamente para el área de sistemas </strong> ";
+
+
+
+
+
+
+    
+}
